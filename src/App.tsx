a6810b592cb39dc02e99475088ec5df4e154b885
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-09-24 14:04:12 -0300

    completed Building Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import { useState } from "react";
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Rice", amount: 10, category: "Groceries" },
+    { id: 2, description: "Coca", amount: 5, category: "Groceries" },
+    { id: 3, description: "Red Car", amount: 15, category: "Entertainment" },
+    { id: 4, description: "T-Shirt", amount: 30, category: "Utilities" },
+    { id: 5, description: "Chair", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <>
-      <Form />
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,25 @@
 import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-/////////////////// USESFORM HOOK ////////////////////////////////
+///////////////////// SCHEMA BASED VALIDATION WITH ZOD ///////////
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+/////////////////// USESFORM HOOK WITH ZOD SCHEMA BASED VALIDATION /////
 const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm(); // React Hook Form uses reference object to get values from input fields
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) }); // React Hook Form uses reference object to get values from input fields
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -18,36 +30,80 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 3 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required.</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 3 characters.</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-lable">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
 
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
   );
 };
+
+/////////////////// USESFORM HOOK ////////////////////////////////
+// const Form = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm(); // React Hook Form uses reference object to get values from input fields
+
+//   const onSubmit = (data: FieldValues) => console.log(data);
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-lable">
+//           Name
+//         </label>
+//         <input
+//           {...register("name", { required: true, minLength: 3 })}
+//           id="name"
+//           type="text"
+//           className="form-control"
+//         />
+//         {errors.name?.type === "required" && (
+//           <p className="text-danger">The name field is required.</p>
+//         )}
+//         {errors.name?.type === "minLength" && (
+//           <p className="text-danger">The name must be at least 3 characters.</p>
+//         )}
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-lable">
+//           Age
+//         </label>
+//         <input
+//           {...register("age")}
+//           id="age"
+//           type="number"
+//           className="form-control"
+//         />
+//       </div>
+
+//       <button className="btn btn-primary" type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// };
 
 /////////////////// USESTATE HOOK ////////////////////////////////
 // const Form = () => {
